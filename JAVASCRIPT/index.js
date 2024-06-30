a018e5f13876aca4fec707461678342cf11c0f10
VivekRanjan620 <143477763+VivekRanjan620@users.noreply.github.com>
2024-07-01 04:05:52 +0530

    add

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,61 @@
+//JAVASCRIPT
+// JavaScript is an interpreted language, not a compiled language. 
+// A program such as C++ or Java needs to be compiled before it is run. 
+// The source code is passed through a program called a compiler, 
+// which translates it into bytecode that the machine understands and can execute.
+
+//PROGRAMMING LANG:- HIGH LEVEL LANG & LOW LEVEL LANG 
+//HIGH LEVEL LANG :-Some examples of high-level languages include Java, C++, COBOL, Pascal, Ruby, etc.
+//LOW LEVEL LANG :- Some examples of low-level languages include the Machine language and Assembly language.(01001)
+
+
+
+// In var re-declare and updation is possible
+// var a = 11;
+// var a = 13;
+// console.log(a);
+
+// In let updation is possible only
+// let b = 12;
+// b = 17;
+// console.log(b);
+
+// In Const cannot be re-declared or updated
+// const c = 15;
+// console.log(c);
+
+// There are two types of scope in JavaScript.These are:-
+//Global scope — Global scope contains all of the things defined outside of all code blocks. 
+// A code block simply consists of grouped statements inside curly braces ({ }). 
+// if statement, loops, function are examples of structure that create a code block.
+//  A global variable has global scope.
+//  A global variable is accessible from anywhere in the code.
+
+//Local Scope — Local scope contains things defined inside code blocks. 
+//A local variable has local scope. A local variable is only accessible where it’s declared.
+
+//let variableName = "A string"; //global scope
+// if (condition) {
+//     block of code to be executed  //local scope 
+//     }
+//     let anotherVariable = "something else"; //global scope
+
+//DATA TYPES
+// JavaScript has the following data types:
+
+// Primitive Types:
+
+// String
+// Number
+// BigInt
+// Boolean
+// Symbol
+// Undefined
+// Null
+
+// Non-Primitive Types:
+
+// Object (including Arrays, Functions, and other objects)
+
+// let num = 20;
+//  console.log(typeof(num));
