b285e9b427659c28a33e708d5255c58653d15d22
VivekRanjan620 <143477763+VivekRanjan620@users.noreply.github.com>
2024-07-03 01:10:01 +0530

    Paytm-Project-UI

---
+++ TALWIND/Project-7-PaytmClone/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      width:{
+        '60p': '60%',
+      },
+      colors:{
+        'nilla': "#00baf2",
+        darkNilla: '#002970',
+        'black1': "#000000",
+        'lightcolor': "f5f5f5",
+      }
+
+    },
+  },
+  plugins: [],
+}
+
