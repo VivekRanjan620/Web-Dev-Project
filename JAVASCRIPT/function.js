4bbfdae5e9ec325e2e32d02366e5a99f1031d5d2
VivekRanjan620 <143477763+VivekRanjan620@users.noreply.github.com>
2024-09-03 15:05:57 +0530

    BOOTSTRAP5

---
+++ JAVASCRIPT/function.js
@@ -0,0 +1,10 @@
+// A JavaScript function is a block of code designed to perform a particular task.
+
+// A JavaScript function is executed when "something" invokes it (calls it).
+
+
+function myfunction(p1,p2){
+    return p1*p2;
+}
+
+let result = myfunction(3,4);
